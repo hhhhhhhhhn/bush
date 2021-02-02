8174f3fc8c75735a165124726ab2a180b7ff9fcd
hhhhhhhhhn <joseugarte2262005@gmail.com>
2021-02-02 02:33:20 -0300

    rewrote the template function to work with any character

--- build.sh
+++ build.sh
@@ -8,23 +8,37 @@ rm -rf "$DIST/"
 
 cp -r "$SRC/" "$DIST/"
 
-IFS=$'\n'
-
-function template() { # writes to /tmp/bush
-	cat | tr "\n" "\f" > /tmp/bush
-	cat /tmp/bush > "$file"
-	for script in `cat "$file" | grep -o "{{[^}]*}}"`; do
-		echo "${script:2:-2}" | tr "\f" "\n" > /tmp/bush.sh
-		chmod +x /tmp/bush.sh
-		. /tmp/bush.sh | tr "\n" "\f" | head -c -1 \
-			| xargs -I {} sed -i "s/${script}/{}/" "$file"
+function template() {
+	IN_BRACKETS=0
+	IN_SCRIPT=0
+	while IFS= read -d '' -rn1 chr; do
+		if [ "$chr" = "{" ]; then
+			if [ "$IN_BRACKETS" = 0 ]; then
+				IN_BRACKETS=1
+			else
+				IN_BRACKETS=0
+				IN_SCRIPT=1
+				printf "" > /tmp/bush
+			fi
+		elif [ "$chr" = "}" ] && [ "$IN_SCRIPT" = 1 ]; then
+			if [ "$IN_BRACKETS" = 0 ]; then
+				IN_BRACKETS=1
+			else
+				IN_BRACKETS=0
+				IN_SCRIPT=0
+				. /tmp/bush | head -c -1
+			fi
+		elif [ "$IN_SCRIPT" = 1 ]; then
+			printf "$chr" >> /tmp/bush
+		else
+			printf "$chr"
+		fi
 	done
-	cat "$file" | tr "\f" "\n" > /tmp/bush
 }
 
 for file in `find -L "$DIST/" -type f -not -path '*/\.*'`; do
-	template <"$file"
-	cat /tmp/bush > "$file"
+	template <"$file" >/tmp/bush2
+	cat /tmp/bush2 > "$file"
 done
 
 for file in `find -L "$DIST/" -type f -path '*/\.*'`; do
